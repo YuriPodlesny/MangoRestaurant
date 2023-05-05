e6dfae00ab4bef2acc5a9621ae9deb746ff319b5
YuriPodlesny <yur1pd@yandex.ru>
2023-05-05 23:54:48 +0500

    Добавьте файлы проекта.

---
+++ Mango.Servrces.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Servrces.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Servrces.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
