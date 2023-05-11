fb61687f8fc9c133232deaf5844df5328e01ed75
YuriPodlesny <yur1pd@yandex.ru>
2023-05-12 01:36:57 +0500

    Настройка приема сообщений и сохранение заказов в базе данных

---
+++ Mango.Services.OrderAPI/Messaging/AsureServiceBusCunsumer.cs
@@ -0,0 +1,61 @@
+﻿using Azure.Messaging.ServiceBus;
+using Mango.Services.OrderAPI.Messages;
+using Mango.Services.OrderAPI.Models;
+using Mango.Services.OrderAPI.Repository;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Mango.Services.OrderAPI.Messaging
+{
+    public class AsureServiceBusCunsumer
+    {
+        private readonly OrderRepository _orderRepository;
+
+        public AsureServiceBusCunsumer(OrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        private async Task OnecheckOutMassegeReceive(ProcessMessageEventArgs args)
+        {
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            CheckoutHeaderDto checkoutHeaderDto = JsonConvert.DeserializeObject<CheckoutHeaderDto>(body);
+
+            OrderHeader orderHeader = new()
+            {
+                UserId = checkoutHeaderDto.UserId,
+                FirstName = checkoutHeaderDto.FirstName,
+                LastName = checkoutHeaderDto.LastName,
+                OrderDetails = new List<OrderDetails>(),
+                CartNumber = checkoutHeaderDto.CartNumber,
+                CouponCode = checkoutHeaderDto.CouponCode,
+                CVV = checkoutHeaderDto.CVV,
+                DiscountTotal = checkoutHeaderDto.DiscountTotal,
+                Email = checkoutHeaderDto.Email,
+                ExpiryMonthYear = checkoutHeaderDto.ExpiryMonthYear,
+                OrderTime = DateTime.Now,
+                OrderTotal = checkoutHeaderDto.OrderTotal,
+                PaymentStatus = false,
+                Phone = checkoutHeaderDto.Phone,
+                PickupDateTime = checkoutHeaderDto.PickupDateTime
+            };
+            foreach (var detailList in checkoutHeaderDto.CartDetails)
+            {
+                OrderDetails orderDetails = new()
+                {
+                    ProductId = detailList.ProductId,
+                    ProductName = detailList.Product.Name,
+                    Price = detailList.Product.Price,
+                    Count = detailList.Count
+                };
+                orderHeader.CartTotalItems += detailList.Count;
+                orderHeader.OrderDetails.Add(orderDetails);
+            }
+            await _orderRepository.AddOrder(orderHeader);
+
+        }
+    }
+}
